f7ed3afea726332ed8e86c485410222fa8c09e5c
Kaique da Silva Ivo <kaiqueivo1011@gmail.com>
2024-10-30 23:51:43 -0300

    Definindo símbolos para o container de injeção de dependências

--- src/di/identifiers.ts
+++ src/di/identifiers.ts
@@ -7,24 +7,37 @@ export abstract class Identifier {
     public static readonly APP: any = Symbol.for('App')
 
     // Controllers
-    public static readonly DIRECTORY_CONTROLLER: any = Symbol.for('DirectoryController')
-    public static readonly FILE_CONTROLLER: any = Symbol.for('FileController')
+    public static readonly USERS_CONTROLLER: any = Symbol.for('UsersController')
+    public static readonly ADMIN_CONTROLLER: any = Symbol.for('AdminController')
+    public static readonly CLIENT_CONTROLLER: any = Symbol.for('ClientController')
+    public static readonly EMPLOYEE_CONTROLLER: any = Symbol.for('EmployeeController')
+    public static readonly AUTH_CONTROLLER: any = Symbol.for('AuthController')
 
     // Services
-    public static readonly DIRECTORY_SERVICE: any = Symbol.for('DirectoryService')
-    public static readonly FILE_SERVICE: any = Symbol.for('FileService')
+    public static readonly USER_SERVICE: any = Symbol.for('UserService')
+    public static readonly ADMIN_SERVICE: any = Symbol.for('AdminService')
+    public static readonly CLIENT_SERVICE: any = Symbol.for('ClientService')
+    public static readonly EMPLOYEE_SERVICE: any = Symbol.for('EmployeeService')
+    public static readonly AUTH_SERVICE: any = Symbol.for('AuthService')
 
     // Repositories
+    public static readonly AUTH_REPOSITORY: any = Symbol.for('AuthRepository')
+    public static readonly GATEWAY_REPOSITORY: any = Symbol.for('GatewayRepository')
+    public static readonly USER_REPOSITORY: any = Symbol.for('UserRepository')
     public static readonly INTEGRATION_EVENT_REPOSITORY: any = Symbol.for('IntegrationEventRepository')
-    public static readonly DIRECTORY_REPOSITORY: any = Symbol.for('DirectoryRepository')
-    public static readonly FILE_REPOSITORY: any = Symbol.for('FileRepository')
+    public static readonly ADMIN_REPOSITORY: any = Symbol.for('AdminRepository')
+    public static readonly CLIENT_REPOSITORY: any = Symbol.for('ClientRepository')
+    public static readonly EMPLOYEE_REPOSITORY: any = Symbol.for('EmployeeRepository')
 
     // Models
     public static readonly INTEGRATION_EVENT_REPO_MODEL: any = Symbol.for('IntegrationEventRepoModel')
-    public static readonly DIRECTORY_REPO_MODEL: any = Symbol.for('DirectoryRepoModel')
+    public static readonly USER_REPO_MODEL: any = Symbol.for('UserRepoModel')
 
     // Mappers
-    public static readonly DIRECTORY_MAPPER: any = Symbol.for('DirectoryMapper')
+    public static readonly USER_ENTITY_MAPPER: any = Symbol.for('UserEntityMapper')
+    public static readonly ADMIN_ENTITY_MAPPER: any = Symbol.for('AdminEntityMapper')
+    public static readonly CLIENT_ENTITY_MAPPER: any = Symbol.for('ClientEntityMapper')
+    public static readonly EMPLOYEE_ENTITY_MAPPER: any = Symbol.for('EmployeeEntityMapper')
 
     // Background Services
     public static readonly MONGODB_CONNECTION_FACTORY: any = Symbol.for('ConnectionFactoryMongodb')
@@ -34,8 +47,8 @@ export abstract class Identifier {
     public static readonly RABBITMQ_CONNECTION: any = Symbol.for('ConnectionRabbitMQ')
     public static readonly RABBITMQ_EVENT_BUS: any = Symbol.for('EventBusRabbitMQ')
 
-
     // Tasks
+    public static readonly REGISTER_DEFAULT_ADMIN_TASK: any = Symbol.for('RegisterDefaultAdminTask')
     public static readonly PUBLISH_EVENT_BUS_TASK: any = Symbol.for('PublishEventBusTask')
     public static readonly SUBSCRIBE_EVENT_BUS_TASK: any = Symbol.for('SubscribeEventBusTask')
     public static readonly RPC_SERVER_EVENT_BUS_TASK: any = Symbol.for('RpcServerEventBusTask')
